f1f26c06ab51207c6dfe4aab5fd76000dd55d4bf
Nikolas Žilić <zilicnikolas@gmail.com>
2026-05-19 15:36:06 +0200

    Add delete kickoff by id route

--- backend/src/routes/kickoffHistory.routes.js
+++ backend/src/routes/kickoffHistory.routes.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import { connectDB } from '../db.js';
+import { ObjectId } from 'mongodb';
 
 const router = express.Router();
 
@@ -58,6 +59,28 @@ router.delete('/', async (req, res) => {
   });
  }catch(error){
   res.status(500).json({error: 'Can not delete all history. Please try again later.'});
+ }
+});
+
+router.delete('/:id', async (req,res) => {
+ try{
+  const history = await getKickoffCollection();
+  const kickoffId = req.params.id;
+ 
+  const kickoffIdObject = new ObjectId(kickoffId);
+  const deleteOneKickoff =  await history.deleteOne({_id: kickoffIdObject});
+
+  if(!deleteOneKickoff.deletedCount){
+    res.status(404).json({error:'The kickoff could not be found'});
+    return
+  };
+
+  res.status(200).json({
+   message: 'Kickoff deleted successfully',
+   deletedCount: deleteOneKickoff.deletedCount
+  });
+ } catch(error){
+  res.status(500).json({error: 'Kickoff can not be deleted.'});
  }
 });
 
