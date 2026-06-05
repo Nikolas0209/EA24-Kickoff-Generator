c07b8b6bf70d94ea22ab1f6da6b645bf1cd8bf91
Nikolas Žilić <zilicnikolas@gmail.com>
2026-06-05 15:34:30 +0200

    Create initial frontend layout and header

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,24 @@
-import './App.css'
+import './App.css';
+import eaLogo from './assets/eaLogo.png';
+import ball from './assets/ball.png';
 
 function App() {
   
   return (
     <>
+     <header className="header-div">
+      <div className="header-section-1">
+        <img src={eaLogo} alt="ea-logo" className="game-logo"/>
+      </div>
+      <div className="header-section-2">
+       <h3 className="title">
+        EA Kickoff Generator
+       </h3>
+      </div>
+      <div className="header-section-3">
+        <img src={ball} className="ball"/>
+      </div>
+     </header>
     </>
   )
 }
