84b87dc0f3e19bb7f6ab5cdfcff418a9de287b58
Nikolas Žilić <zilicnikolas@gmail.com>
2026-06-05 17:52:05 +0200

    Fetch international kickoff data from backend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,31 @@
 import './App.css';
 import eaLogo from './assets/eaLogo.png';
 import ball from './assets/ball.png';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
-function App() {
-  
+type InternationalKickoff = {
+  country: string,
+  stars: number,
+  logo: string
+}
+
+function App(){
+  const [countriesKickoff, setCountriesKickoff] = useState<InternationalKickoff[]>([]);
+
+  useEffect(() => {
+    const fetchInternationalKickoff = async (): Promise<void> => {
+     try{
+      const response = await axios.get('http://localhost:3000/countries');
+      setCountriesKickoff(response.data);
+     } catch(error){
+      console.log('Could not fetch the Internationals', error)
+     }
+    }
+
+    fetchInternationalKickoff();
+  }, []);
+
   return (
     <>
      <header className="header-div">
