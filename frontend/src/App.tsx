a97a47f0b67d47c7d1b7b619dae51d550a069a9f
Nikolas Žilić <zilicnikolas@gmail.com>
2026-06-06 15:22:59 +0200

    Separate appheader from app component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,12 @@
 import './App.css';
-import eaLogo from './assets/eaLogo.png';
-import ball from './assets/ball.png';
+import AppHeader from './pages/AppHeader';
 import HomePage from './pages/HomePage';
 //import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App(){
  return (
   <>
-   <header className="header-div">
-    <div className="header-section-1">
-      <img src={eaLogo} alt="ea-logo" className="game-logo"/>
-    </div>
-    <div className="header-section-2">
-     <h3 className="title">
-       EA Kickoff Generator
-     </h3>
-    </div>
-    <div className="header-section-3">
-     <img src={ball} className="ball"/>
-    </div>
-   </header>
-
+   <AppHeader/>
    <HomePage />
   </>
  )
