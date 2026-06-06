73524e698e2be43255149feefc409e11572ca8c9
Nikolas Žilić <zilicnikolas@gmail.com>
2026-06-06 16:31:47 +0200

    Move fetch countries kickoff in international kickoff component

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,31 +1,9 @@
 import './HomePage.css';
-import { useState, useEffect } from 'react';
-import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 
-type InternationalKickoff = {
-  country: string,
-  stars: number,
-  logo: string
-}
-
 function HomePage(){
-  const [countriesKickoff, setCountriesKickoff] = useState<InternationalKickoff[]>([]);
   const navigate = useNavigate();
  
-  useEffect(() => {
-    const fetchInternationalKickoff = async (): Promise<void> => {
-     try{
-      const response = await axios.get('http://localhost:3000/countries');
-      setCountriesKickoff(response.data);
-     } catch(error){
-      console.log('Could not fetch the Internationals', error)
-     }
-    }
-
-    fetchInternationalKickoff();
-  }, []);
-
   const navigatePage = (): void => {
     navigate('/countries')
   }
--- frontend/src/pages/InternationalKickoff.tsx
+++ frontend/src/pages/InternationalKickoff.tsx
@@ -1,8 +1,31 @@
 import './InternationalKickoff.css';
 import { useNavigate } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
+type InternationalKickoff = {
+  country: string,
+  stars: number,
+  logo: string
+}
+
 
 function InternationalKickoff(){
  const navigate = useNavigate();
+ const [countriesKickoff, setCountriesKickoff] = useState<InternationalKickoff[]>([]);
+ 
+ useEffect(() => {
+   const fetchInternationalKickoff = async (): Promise<void> => {
+    try{
+     const response = await axios.get('http://localhost:3000/countries');
+     setCountriesKickoff(response.data);
+    } catch(error){
+     console.log('Could not fetch the Internationals', error)
+    }
+   }
+
+   fetchInternationalKickoff();
+ }, []);
 
  const navigatePage = (): void => {
   navigate('/')
