9b68a8c44fd7798ac527ff54fa319c9bc86473ee
Nikolas Žilić <zilicnikolas@gmail.com>
2026-06-11 01:27:36 +0200

    Move starRatings into its own file

---
+++ frontend/src/data/starRatings.ts
@@ -0,0 +1,23 @@
+import rating_05 from '../assets/starRatings/rating_05.png';
+import rating_1 from '../assets/starRatings/rating_1.png';
+import rating_1_5 from '../assets/starRatings/rating_1_5.png';
+import rating_2 from '../assets/starRatings/rating_2.png';
+import rating_2_5 from '../assets/starRatings/rating_2_5.png';
+import rating_3 from '../assets/starRatings/rating_3.png';
+import rating_3_5 from '../assets/starRatings/rating_3_5.png';
+import rating_4 from '../assets/starRatings/rating_4.png';
+import rating_4_5 from '../assets/starRatings/rating_4_5.png';
+import rating_5 from '../assets/starRatings/rating_5.png';
+
+export  const starRatings = {
+  0.5: rating_05,
+  1: rating_1,
+  1.5: rating_1_5,
+  2: rating_2,
+  2.5: rating_2_5,
+  3: rating_3,
+  3.5: rating_3_5,
+  4: rating_4,
+  4.5: rating_4_5,
+  5: rating_5
+ };
--- frontend/src/pages/InternationalKickoff.tsx
+++ frontend/src/pages/InternationalKickoff.tsx
@@ -2,17 +2,7 @@ import './InternationalKickoff.css';
 import { useNavigate } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
-import rating_05 from '../assets/starRatings/rating_05.png';
-import rating_1 from '../assets/starRatings/rarting_1.png';
-import rating_1_5 from '../assets/starRatings/rating_1_5.png';
-import rating_2 from '../assets/starRatings/rating_2.png';
-import rating_2_5 from '../assets/starRatings/rating_2_5.png';
-import rating_3 from '../assets/starRatings/rating_3.png';
-import rating_3_5 from '../assets/starRatings/rating_3_5.png';
-import rating_4 from '../assets/starRatings/rating_4.png';
-import rating_4_5 from '../assets/starRatings/rating_4_5.png';
-import rating_5 from '../assets/starRatings/rating_5.png';
-
+import { starRatings } from '../data/starRatings'; 
 
 type Team = {
   _id: string,
@@ -46,19 +36,6 @@ function InternationalKickoff(){
 
  const navigatePage = (): void => {
   navigate('/')
- }
-
- const starRatings = {
-  0.5: rating_05,
-  1: rating_1,
-  1.5: rating_1_5,
-  2: rating_2,
-  2.5: rating_2_5,
-  3: rating_3,
-  3.5: rating_3_5,
-  4: rating_4,
-  4.5: rating_4_5,
-  5: rating_5
  }
 
  return(
