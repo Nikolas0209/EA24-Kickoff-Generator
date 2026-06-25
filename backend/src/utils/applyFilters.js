720d4c80252d8950bb46ada42fb1afb48a25b763
Nikolas Žilić <zilicnikolas@gmail.com>
2026-06-26 00:57:30 +0200

    Handle null values safely with optional chaining in toString call

--- backend/src/utils/applyFilters.js
+++ backend/src/utils/applyFilters.js
@@ -1,6 +1,6 @@
 function applyFilters(teams, {competition, league}){
   if(competition){
-   return teams.filter(team => team.competition.toUpperCase() === competition.toUpperCase());
+   return teams.filter(team => team.competition?.toUpperCase() === competition.toUpperCase());
   }else if(league){
    return teams.filter(team => team.league === league);
   }else {
