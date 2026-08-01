806ca806b29479a622e17efc1fa6907e219c6988
Nikolas Žilić <zilicnikolas@gmail.com>
2026-08-01 16:10:54 +0200

    Add retry fetch functionality to useKickoff hook

--- frontend/src/hooks/useKickoff.ts
+++ frontend/src/hooks/useKickoff.ts
@@ -4,18 +4,26 @@ import axios from "axios";
 export function useKickoff <T>(url: string, autofetch = true){
   const [kickoff, setKickoff] = useState<T | null>(null);
   const [isLoading, setIsLoading] = useState<boolean>(false);
-
+  const [hasError, setHasError] = useState<boolean>(false);
+  
   const fetchKickoff = useCallback(async (customUrl?: string) => {
     try {
+      setHasError(false);
       setIsLoading(true);
       const response = await axios.get<T>(customUrl ?? url);
       setKickoff(response.data);
     } catch (error) {
       console.log("Could not fetch the Kickoff", error);
+      setHasError(true);
     } finally{
       setIsLoading(false);
     }
   }, [url]);
+
+  const retryFetch = async (): Promise<void> => {
+    setKickoff(null)
+    await fetchKickoff();
+  }
   
   useEffect(() => {
     if(autofetch){
@@ -23,5 +31,5 @@ export function useKickoff <T>(url: string, autofetch = true){
     }
   }, [fetchKickoff, autofetch]);
 
-  return { kickoff, setKickoff, fetchKickoff, isLoading }
+  return { kickoff, setKickoff, fetchKickoff, isLoading, hasError, retryFetch };
 }; 
