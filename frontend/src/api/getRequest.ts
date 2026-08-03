e56ca3a636758d9d54e20c44d53391d7f70f6dfc
Nikolas Žilić <zilicnikolas@gmail.com>
2026-08-03 18:39:27 +0200

    Improve get request error handling

--- frontend/src/api/getRequest.ts
+++ frontend/src/api/getRequest.ts
@@ -3,9 +3,9 @@ import axios from "axios";
 export const getRequest = async <T>(url: string): Promise<T> => {
   try{
     const response = await axios.get<T>(url);
-    return response.data
+    return response.data;
   }catch(error){
-    console.log('Could not fetch a new team', error);
-    throw error
+    console.log('GET request failed', error);
+    throw error;
   }
 }
